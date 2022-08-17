740a3fb67ec796bf03e695cb776ab61d834b3205
Mark Lyttle <mark.lyttle@imperva.com>
2022-08-17 17:29:11 +0100

    Make public

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,12 +1,18 @@
-public struct Dog {
-    var age = 8
-    var name = "Roger"
 
-    func bark() {
+public class Dog {
+    public let age: Int
+    public let name: String
+
+    public init( age: Int, name: String) {
+        self.age = age
+        self.name = name
+    }
+    
+    public func bark() {
         print("\(name): wof!")
     }
 
-    static func hello() {
-        print("Hello I am the Dog struct")
+    public static func hello() {
+        print("Hello I am the Dog class")
     }
 }
