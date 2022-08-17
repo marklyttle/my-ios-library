22de89cfb3eda92d4822fd1e05dde566a6ded5f0
Mark Lyttle <mark.lyttle@imperva.com>
2022-08-17 17:02:23 +0100

    More useful content

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,12 @@
-struct MyLibrary {
-    var text = "Hello, World!"
+struct Dog {
+    var age = 8
+    var name = "Roger"
+
+    func bark() {
+        print("\(name): wof!")
+    }
+
+    static func hello() {
+        print("Hello I am the Dog struct")
+    }
 }
